883dddde9ff64dd572dc7ea2992ccbf1ffa2bf92
Son Do <dominhson580@gmail.com>
2023-02-05 22:39:57 +0700

    Include header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
