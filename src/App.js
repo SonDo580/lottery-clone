94efa7a28769c36e1e71bcb0a59d43097bb6014f
Son Do <dominhson580@gmail.com>
2023-02-06 16:37:09 +0700

    Add component Chat

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useMediaQuery } from "react-responsive";
 import "./App.scss";
+import Chat from "./components/Chat";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
@@ -16,6 +17,7 @@ function App() {
       {zeroToLargeScreen ? null : <NavBar />}
 
       <Footer />
+      <Chat />
     </div>
   );
 }
