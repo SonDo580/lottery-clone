59df98a2f19e9ae860c20a53b6fbb3aa790bd820
Son Do <dominhson580@gmail.com>
2023-02-06 22:36:44 +0700

    Add Intro section

---
+++ src/pages/Home/components/Intro/index.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { Images } from "../../../../images";
+
+function index() {
+  return (
+    <section className="intro">
+      <div className="left">
+        <div className="card">
+          <div className="card-img">
+            <img src={Images.home.intro.globe} alt="globe" />
+          </div>
+          <div className="card-text">
+            <p>
+              <strong>
+                Onbit<span>VN</span>
+              </strong>{" "}
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, enim?
+            </p>
+            <a href="/">
+              <img src={Images.home.intro.arrow} alt="arrow-icon" />
+              <span>Who we are?</span>
+            </a>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-img">
+            <img src={Images.home.intro.give} alt="give-online" />
+          </div>
+          <div className="card-text">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+              ex laudantium, fuga pariatur deleniti, exercitationem nisi sed
+              obcaecati repudiandae sint ipsum maiores
+            </p>
+            <a href="/">
+              <img src={Images.home.intro.arrow} alt="arrow-icon" />
+              <span>How to play</span>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className="right">
+        <iframe
+          width="500"
+          height="300"
+          src="https://www.youtube.com/embed/nltt3i5X7FU"
+          title="Onbit.vn"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
+    </section>
+  );
+}
+
+export default index;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,6 +4,7 @@ import { Screen } from "../../style/screen";
 import Contact from "./components/Contact";
 import Feature from "./components/Feature";
 import Process from "./components/Process";
+import Intro from "./components/Intro";
 
 function Home() {
   const toMobileScreen = useMediaQuery({
@@ -14,6 +15,7 @@ function Home() {
     <div>
       <Banner />
       <Process />
+      <Intro />
       <Feature />
       {toMobileScreen ? <Contact /> : null}
     </div>
