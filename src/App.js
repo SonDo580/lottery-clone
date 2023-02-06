5c53fac7dd848fa2ace62399baf149fe65f74d17
Son Do <dominhson580@gmail.com>
2023-02-06 15:08:40 +0700

    Add NavBar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.scss";
 import Header from "./components/Header";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <NavBar />
     </div>
   );
 }
