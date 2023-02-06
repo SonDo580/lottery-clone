f593c9270d44c7225fb11f6c26e7eba086a35d58
Son Do <dominhson580@gmail.com>
2023-02-06 15:27:06 +0700

    Extract HeaderRight component

---
+++ src/components/Header/components/HeaderRight.js
@@ -0,0 +1,30 @@
+import { Images } from "../../../images";
+
+function HeaderRight() {
+  return (
+    <div className="header-right">
+      <div className="top-line">
+        <input type="text" placeholder="Email or Phone" />
+        <input type="password" placeholder="Password" />
+        <button className="login">Log in</button>
+        <button className="signup">Sign up</button>
+      </div>
+      <p>
+        <a href="/">Forgot Password</a>
+      </p>
+      <div className="bottom-line">
+        <button className="share">Sharing Royalty</button>
+        <button className="fb-login">
+          <img src={Images.header.facebook} alt="facebook icon" />
+          Login with Facebook
+        </button>
+        <button className="zalo-login">
+          <img src={Images.header.zalo} alt="zalo icon" />
+          Login with Zalo
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default HeaderRight;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,7 @@
 import { useMediaQuery } from "react-responsive";
 import { Images } from "../../images";
 import { Screen } from "../../style/screen";
+import HeaderRight from "./components/HeaderRight";
 import MobileMenu from "./components/MobileMenu";
 import "./Header.scss";
 
@@ -17,30 +18,7 @@ function Header() {
         </a>
       </div>
 
-      {isLargeScreen ? null : (
-        <div className="header-right">
-          <div className="top-line">
-            <input type="text" placeholder="Email or Phone" />
-            <input type="password" placeholder="Password" />
-            <button className="login">Log in</button>
-            <button className="signup">Sign up</button>
-          </div>
-          <p>
-            <a href="/">Forgot Password</a>
-          </p>
-          <div className="bottom-line">
-            <button className="share">Royalty</button>
-            <button className="fb-login">
-              <img src={Images.header.facebook} alt="facebook icon" />
-              Login with Facebook
-            </button>
-            <button className="zalo-login">
-              <img src={Images.header.zalo} alt="zalo icon" />
-              Login with Zalo
-            </button>
-          </div>
-        </div>
-      )}
+      {isLargeScreen ? null : <HeaderRight />}
 
       {isLargeScreen ? <MobileMenu /> : null}
     </header>
