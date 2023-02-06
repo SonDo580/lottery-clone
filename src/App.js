884ee3a7f648d45535e01bf5084a17af0bc78efc
Son Do <dominhson580@gmail.com>
2023-02-06 21:05:28 +0700

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { useMediaQuery } from "react-responsive";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.scss";
 import Chat from "./components/Chat";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import Play from "./pages/Play";
+import Result from "./pages/Result";
 import { Screen } from "./style/screen";
 
 function App() {
@@ -12,13 +16,21 @@ function App() {
   });
 
   return (
-    <div className="app">
-      <Header />
-      {toLargeScreen ? null : <NavBar />}
+    <BrowserRouter>
+      <div className="app">
+        <Header />
+        {toLargeScreen ? null : <NavBar />}
 
-      <Footer />
-      <Chat />
-    </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/play" element={<Play />} />
+          <Route path="/result" element={<Result />} />
+        </Routes>
+
+        <Footer />
+        <Chat />
+      </div>
+    </BrowserRouter>
   );
 }
 
