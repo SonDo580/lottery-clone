2d661952fd647a7aecb9df18fd20948b0c704d12
Son Do <dominhson580@gmail.com>
2023-02-06 21:11:45 +0700

    Change spacing

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,7 +1,14 @@
 function NotFound() {
   return (
-    <div>
-      <h1>Page Not Found</h1>
+    <div
+      style={{
+        height: "40vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h1 style={{ fontSize: "3rem" }}>Page Not Found</h1>
     </div>
   );
 }
