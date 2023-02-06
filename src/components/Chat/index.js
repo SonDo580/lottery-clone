c89ffbe9ae5b42108655b172c4ffd4c71d036db9
Son Do <dominhson580@gmail.com>
2023-02-06 16:41:23 +0700

    Define Chat component

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -1,5 +1,25 @@
+import { Images } from "../../images";
+import "./Chat.scss";
+
 function Chat() {
-  return <div>Chat</div>;
+  return (
+    <div class="fixed">
+      <button class="showIcon">
+        <img src={Images.fixed.message} alt="message-icon" />
+      </button>
+
+      <div class="wrapper">
+        <div class="icons">
+          <a href="https://zalo.me/0967401489">
+            <img class="zalo" src={Images.fixed.zalo} alt="zalo-icon" />
+          </a>
+          <a href="https://zalo.me/0967401489">
+            <img class="zalo" src={Images.fixed.zalo} alt="zalo-icon" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Chat;
