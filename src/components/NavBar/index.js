2b8932c82c9d6a2f79b5766b3952667a7b616b9a
Son Do <dominhson580@gmail.com>
2023-02-06 15:14:12 +0700

    Define NavBar component

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,5 +1,58 @@
+import { FaCaretDown } from "react-icons/fa";
+
 function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          Play now <FaCaretDown />
+          <div class="list">
+            <p>
+              <a href="/">Vietlott Keno</a>
+            </p>
+            <p>
+              <a href="/">US MegaMillions</a>
+            </p>
+            <p>
+              <a href="/">Euro Millions</a>
+            </p>
+            <p>
+              <a href="/">Oz Lotto</a>
+            </p>
+          </div>
+        </li>
+        <li>
+          Result <FaCaretDown />
+          <div class="list">
+            <p>
+              <a href="/">Vietlott Keno</a>
+            </p>
+            <p>
+              <a href="/">US MegaMillions</a>
+            </p>
+            <p>
+              <a href="/">Euro Millions</a>
+            </p>
+            <p>
+              <a href="/">Oz Lotto</a>
+            </p>
+          </div>
+        </li>
+        <li>
+          <a href="/">Winners</a>
+        </li>
+        <li>
+          <a href="/">News and Events</a>
+        </li>
+        <li>
+          <a href="/">About us</a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default NavBar;
