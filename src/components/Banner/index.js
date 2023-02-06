f1b00d1f6da6ce5c23fab4c4f4f64107763ee741
Son Do <dominhson580@gmail.com>
2023-02-06 21:39:29 +0700

    Circulate the banners

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,11 +1,28 @@
+import { useEffect, useState } from "react";
 import { Images } from "../../images";
 import "./Banner.scss";
 
 function Banner() {
+  const [active, setActive] = useState(true);
+
+  useEffect(() => {
+    setInterval(() => {
+      setActive((prevActive) => !prevActive);
+    }, 3000);
+  }, []);
+
   return (
     <section className="banner">
-      <img src={Images.banner.banner1} alt="banner1" />
-      <img src={Images.banner.banner2} alt="banner2" />
+      <img
+        className={active ? "active" : ""}
+        src={Images.banner.banner1}
+        alt="banner1"
+      />
+      <img
+        className={active ? "" : "active"}
+        src={Images.banner.banner2}
+        alt="banner2"
+      />
     </section>
   );
 }
