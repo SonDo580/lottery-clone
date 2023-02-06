3e984e4bfcba6e6793f2c1a06fcbf053e53b06e0
Son Do <dominhson580@gmail.com>
2023-02-06 21:00:40 +0700

    Handle showing chat icons

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -1,20 +1,38 @@
+import { useState } from "react";
 import { Images } from "../../images";
 import "./Chat.scss";
 
 function Chat() {
+  const [show, setShow] = useState(false);
+
+  const showChatIcons = () => {
+    setShow(true);
+  };
+
+  const hideChatIcons = (event) => {
+    if (event.target.className.includes("icons")) {
+      return;
+    }
+
+    setShow(false);
+  };
+
   return (
-    <div class="fixed">
-      <button class="showIcon">
+    <div className="fixed">
+      <button className="showIcon" onClick={showChatIcons}>
         <img src={Images.fixed.message} alt="message-icon" />
       </button>
 
-      <div class="wrapper">
-        <div class="icons">
+      <div
+        className={`wrapper ${show ? "active" : ""}`}
+        onClick={hideChatIcons}
+      >
+        <div className={`icons ${show ? "active" : ""}`}>
           <a href="https://zalo.me/0967401489">
-            <img class="zalo" src={Images.fixed.zalo} alt="zalo-icon" />
+            <img className="zalo" src={Images.fixed.zalo} alt="zalo-icon" />
           </a>
           <a href="https://zalo.me/0967401489">
-            <img class="zalo" src={Images.fixed.zalo} alt="zalo-icon" />
+            <img className="zalo" src={Images.fixed.zalo} alt="zalo-icon" />
           </a>
         </div>
       </div>
