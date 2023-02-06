2453b1bb273e18f6e5e79ff959805a1bfbccf24d
Son Do <dominhson580@gmail.com>
2023-02-06 23:19:45 +0700

    Update import

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Header from "./components/Header";
 import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
 import Play from "./pages/Play";
-import Result from "./pages/Result";
 import NotFound from "./pages/NotFound";
 import { Screen } from "./style/screen";
 
