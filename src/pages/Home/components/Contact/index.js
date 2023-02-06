b9bca1e20d0a8b73bd28f8de2d5d5d2e4dc8856b
Son Do <dominhson580@gmail.com>
2023-02-06 21:54:59 +0700

    Style Contact section

--- src/pages/Home/components/Contact/index.js
+++ src/pages/Home/components/Contact/index.js
@@ -1,4 +1,4 @@
-import { FaEnvelope, FaPhone } from "react-icons/fa";
+import { FaEnvelope, FaLocationArrow, FaPhone } from "react-icons/fa";
 
 function Contact() {
   return (
@@ -7,6 +7,7 @@ function Contact() {
       <div className="contact-info">
         <p>
           <i className="fa-solid fa-location-dot"></i>
+          <FaLocationArrow />
           <span>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis,
             saepe?
@@ -26,6 +27,7 @@ function Contact() {
           title="Map"
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.523432256371!2d106.68434684944394!3d10.771164262200724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752f2319562955%3A0xf7d5e643bcab925b!2zNDEyIE5ndXnhu4VuIFRo4buLIE1pbmggS2hhaSwgUGjGsOG7nW5nIDUsIFF14bqtbiAzLCBUaMOgbmggcGjhu5EgSOG7kyBDaMOtIE1pbmggNzAwMDAsIFZpZXRuYW0!5e0!3m2!1sen!2s!4v1665472711596!5m2!1sen!2s"
           height="200"
+          width="100%"
           style={{ border: 0 }}
           allowfullscreen=""
           loading="lazy"
