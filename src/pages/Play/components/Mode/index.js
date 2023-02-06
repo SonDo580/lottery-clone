5fa1d06eb34ae084966369ea89f71ec2724c6f6a
Son Do <dominhson580@gmail.com>
2023-02-06 23:58:18 +0700

    Extract Mode component

---
+++ src/pages/Play/components/Mode/index.js
@@ -0,0 +1,59 @@
+import { Images } from "../../../../images";
+
+function Mode() {
+  return (
+    <div className="chooseMode">
+      <h2>
+        <span>Step 3:</span> Choose play mode
+      </h2>
+      <ul>
+        <li>
+          <label for="one">
+            <input type="radio" name="mode" id="oneTime" checked />
+            Once
+          </label>
+        </li>
+
+        <li>
+          <label for="multipleTimes">
+            <input type="radio" name="mode" id="multipleTimes" />
+            Multiple Times
+            <img src={Images.play.question} alt="question mark" />
+            <p className="comment">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod
+              praesentium consequatur cum!
+            </p>
+          </label>
+          <select name="times" id="times">
+            <option value="0">2 times</option>
+            <option value="1">4 times</option>
+            <option value="2">8 times</option>
+          </select>
+        </li>
+
+        <li>
+          <label for="longTerm">
+            <input type="radio" name="mode" id="longTerm" />
+            Long Term
+            <img src={Images.play.question} alt="question mark" />
+            <p className="comment">
+              Lorem ipsum dolor sit amet consectetur: <br />
+              - Lorem ipsum dolor sit amet. <br />
+              - Lorem ipsum dolor sit amet. <br />
+              - Lorem ipsum dolor sit amet. <br />
+              <em>*Lorem ipsum dolor sit amet consectetur adipisicing.</em>
+            </p>
+          </label>
+          <select name="term" id="term">
+            <option value="0">Every quarter</option>
+            <option value="1">Every week</option>
+            <option value="2">Every month</option>
+          </select>
+          <p className="note">* Free for 10th time</p>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Mode;
--- src/pages/Play/index.js
+++ src/pages/Play/index.js
@@ -1,4 +1,5 @@
 import Lottery from "./components/Lottery";
+import Mode from "./components/Mode";
 import Number from "./components/Number";
 import Ticket from "./components/Ticket";
 
@@ -8,58 +9,7 @@ function Play() {
       <Lottery />
       <Ticket />
       <Number />
-
-      <div className="chooseMode">
-        <h2>
-          <span>Bước 3:</span> Chọn chế độ chơi
-        </h2>
-        <ul>
-          <li>
-            <label for="one">
-              <input type="radio" name="mode" id="oneTime" checked />
-              Một lần
-            </label>
-          </li>
-          <li>
-            <label for="multipleTimes">
-              <input type="radio" name="mode" id="multipleTimes" />
-              Nhiều kỳ
-              <img src="./image/play/question.png" alt="question mark" />
-              <p className="comment">
-                Sử dụng bộ số đã chọn cho 5, 10 hoặc 20 lần chơi tiếp theo với
-                nhiều ưu đãi và giảm giá hấp dẫn.
-              </p>
-            </label>
-            <select name="times" id="times">
-              <option value="0">2 kỳ</option>
-              <option value="1">4 kỳ</option>
-              <option value="2">8 kỳ</option>
-            </select>
-          </li>
-          <li>
-            <label for="longTerm">
-              <input type="radio" name="mode" id="longTerm" />
-              Dài hạn
-              <img src="./image/play/question.png" alt="question mark" />
-              <p className="comment">
-                Tự động sử dụng bộ số vừa chọn cho: <br />
-                - Tất cả các kỳ tiếp theo (Hàng kỳ) <br />
-                - Kỳ đầu tiên mỗi tuần (Hàng tuần) <br />
-                - Kỳ đầu tiên mỗi tháng (Hàng tháng) <br />
-                <em>
-                  *Lưu ý, bạn cần nạp đồng Onbit để thực hiện chức năng này.
-                </em>
-              </p>
-            </label>
-            <select name="term" id="term">
-              <option value="0">Hàng kỳ</option>
-              <option value="1">Hàng tuần</option>
-              <option value="2">Hàng tháng</option>
-            </select>
-            <p className="note">* Miễn phí mọi lần chơi thứ 10</p>
-          </li>
-        </ul>
-      </div>
+      <Mode />
 
       <div className="confirmAndPay">
         <h2>
