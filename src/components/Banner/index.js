3979604f3f2015eba3b14e5b202a07d180e9f356
Son Do <dominhson580@gmail.com>
2023-02-06 21:16:20 +0700

    Create Banner component

---
+++ src/components/Banner/index.js
@@ -0,0 +1,12 @@
+import { Images } from "../../images";
+
+function Banner() {
+  return (
+    <section className="banner">
+      <img src={Images.banner.banner1} alt="banner1" />
+      <img src={Images.banner.banner2} alt="banner2" />
+    </section>
+  );
+}
+
+export default Banner;
