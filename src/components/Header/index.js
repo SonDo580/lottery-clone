294d8a612126431d559f2959d2a1aecef1b9f256
Son Do <dominhson580@gmail.com>
2023-02-06 14:45:10 +0700

    Use conditional rendering

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -17,28 +17,30 @@ function Header() {
         </a>
       </div>
 
-      <div className="header-right">
-        <div className="top-line">
-          <input type="text" placeholder="Email or Phone" />
-          <input type="password" placeholder="Password" />
-          <button className="login">Log in</button>
-          <button className="signup">Sign up</button>
+      {isLargeScreen ? null : (
+        <div className="header-right">
+          <div className="top-line">
+            <input type="text" placeholder="Email or Phone" />
+            <input type="password" placeholder="Password" />
+            <button className="login">Log in</button>
+            <button className="signup">Sign up</button>
+          </div>
+          <p>
+            <a href="/">Forgot Password</a>
+          </p>
+          <div className="bottom-line">
+            <button className="share">Royalty</button>
+            <button className="fb-login">
+              <img src={Images.header.facebook} alt="facebook icon" />
+              Login with Facebook
+            </button>
+            <button className="zalo-login">
+              <img src={Images.header.zalo} alt="zalo icon" />
+              Login with Zalo
+            </button>
+          </div>
         </div>
-        <p>
-          <a href="/">Forgot Password</a>
-        </p>
-        <div className="bottom-line">
-          <button className="share">Royalty</button>
-          <button className="fb-login">
-            <img src={Images.header.facebook} alt="facebook icon" />
-            Login with Facebook
-          </button>
-          <button className="zalo-login">
-            <img src={Images.header.zalo} alt="zalo icon" />
-            Login with Zalo
-          </button>
-        </div>
-      </div>
+      )}
 
       {isLargeScreen ? <MobileMenu /> : null}
     </header>
