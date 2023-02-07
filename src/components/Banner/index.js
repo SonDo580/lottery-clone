4edfa85d8e33d990bb3b3d404bb4732b4e6555ba
Son Do <dominhson580@gmail.com>
2023-02-07 11:04:36 +0700

    Create interval when Banner unmount

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -7,16 +7,20 @@ const banners = [Images.banner.banner1, Images.banner.banner2];
 function Banner() {
   const [bannerIndex, setBannerIndex] = useState(0);
 
+  const getNextBanner = () => {
+    setBannerIndex((prevBannerIndex) => {
+      if (prevBannerIndex === banners.length - 1) {
+        return 0;
+      }
+      return prevBannerIndex + 1;
+    });
+  };
+
   useEffect(() => {
-    setInterval(() => {
-      setBannerIndex((prevBannerIndex) => {
-        if (prevBannerIndex === banners.length - 1) {
-          return 0;
-        }
-        return prevBannerIndex + 1;
-      });
-    }, 3000);
-  }, []);
+    const interval = setInterval(() => getNextBanner(), 3000);
+
+    return () => clearInterval(interval);
+  });
 
   return (
     <section className="banner">
