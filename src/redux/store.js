1f31a3ee53eb38eb25cdea01fb57e88d6506541f
Son Do <dominhson580@gmail.com>
2023-02-07 15:10:14 +0700

    Add playSlice to the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import playReducer from "./playSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    play: playReducer,
+  },
 });
