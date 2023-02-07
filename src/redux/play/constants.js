dd4101a3fbdbd1aaa4c4650567f3037a13919f18
Son Do <dominhson580@gmail.com>
2023-02-07 17:01:06 +0700

    Add constant 'table'

--- src/redux/play/constants.js
+++ src/redux/play/constants.js
@@ -2,8 +2,20 @@ const numbers = [1, 3, 5, 10, 15]; // number of tickets
 const price = numbers.map((number) => number * 5); // in dollars
 const discount = [0, 3, 5, 7, 10]; // in percentages
 
+const table = {
+  table1: {
+    numsInTable: 50,
+    maxSelect: 5,
+  },
+  table2: {
+    numsInTable: 12,
+    maxSelect: 2,
+  },
+};
+
 export const ticketConstants = {
   numbers,
   price,
   discount,
+  table,
 };
