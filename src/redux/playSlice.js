3373e9518a848c68594b486454aa33f0627a31cd
Son Do <dominhson580@gmail.com>
2023-02-07 15:09:36 +0700

    Create playSlice

---
+++ src/redux/playSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [];
+
+const playSlice = createSlice({
+  name: "play",
+  initialState,
+  reducers: {},
+});
+
+export default playSlice.reducer;
