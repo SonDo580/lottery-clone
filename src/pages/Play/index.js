2df31856f9b6231b9419e2364b2b59e16d141df2
Son Do <dominhson580@gmail.com>
2023-02-07 16:12:37 +0700

    Remove state and props passing

--- src/pages/Play/index.js
+++ src/pages/Play/index.js
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import Confirm from "./components/Confirm";
 import Lottery from "./components/Lottery";
 import Mode from "./components/Mode";
@@ -6,36 +5,14 @@ import Number from "./components/Number";
 import Ticket from "./components/Ticket";
 import "./Play.scss";
 
-const arrayValues = [1, 3, 5, 10, 15]; // number of tickets
-const arrayPrice = arrayValues.map((value) => value * 5); // in dollars
-const arrayDiscount = [0, 3, 5, 7, 10]; // in percentages
-
 function Play() {
-  const [numTickets, setNumTickets] = useState(arrayValues[1]);
-
-  const indexValue = arrayValues.findIndex((value) => value === numTickets);
-  const price = arrayPrice[indexValue];
-  const discount = (price * arrayDiscount[indexValue]) / 100;
-
-  const changeNumTickets = (value) => {
-    setNumTickets(value);
-  };
-
   return (
     <>
       <Lottery />
-
-      <Ticket
-        arrayValues={arrayValues}
-        numTickets={numTickets}
-        changeNumTickets={changeNumTickets}
-      />
-
-      <Number numTickets={numTickets} />
-
+      <Ticket />
+      <Number />
       <Mode />
-
-      <Confirm price={price} discount={discount} />
+      <Confirm />
     </>
   );
 }
