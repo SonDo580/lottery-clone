11278b75058a2eb78467af9244a828316444aa80
Son Do <dominhson580@gmail.com>
2023-02-07 11:41:47 +0700

    Fix prop name and add key

--- src/pages/Play/components/Number/Card/index.js
+++ src/pages/Play/components/Number/Card/index.js
@@ -2,25 +2,29 @@ import { FaTrash } from "react-icons/fa";
 
 function Card() {
   return (
-    <div class="card">
-      <div class="buttons">
-        <button class="choose">Quick Select</button>
-        <button class="reset">
+    <div className="card">
+      <div className="buttons">
+        <button className="choose">Quick Select</button>
+        <button className="reset">
           <FaTrash />
         </button>
       </div>
 
       <p>Select 5 numbers</p>
-      <div class="numTable table1">
+      <div className="numTable table1">
         {[...Array(50).keys()].map((num) => (
-          <div class="num">{num}</div>
+          <div key={num} className="num">
+            {num + 1}
+          </div>
         ))}
       </div>
 
       <p>Select 2 numbers</p>
-      <div class="numTable table2">
+      <div className="numTable table2">
         {[...Array(12).keys()].map((num) => (
-          <div class="num">{num}</div>
+          <div key={num} className="num">
+            {num + 1}
+          </div>
         ))}
       </div>
     </div>
